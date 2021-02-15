9607c802fc0c00bbbeaa5e1123ee577164a92f76
2444noman <2444noman@gmail.com>
2021-02-15 13:08:02 +0600

    Second Project commit

---
+++ word_count/word_count.py
@@ -0,0 +1,9 @@
+print("What's on your mind today?")
+
+sentence = input()
+
+# print(sentence.split())
+
+words = len(sentence.split())
+
+print(f"Oh nice, you just told me what's on your mind in {words} words!")
